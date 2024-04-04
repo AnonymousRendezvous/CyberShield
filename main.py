5461bce9c7488e8914ac4ab257d950876d906fff
AnonymousRendezvous <92633933+AnonymousRendezvous@users.noreply.github.com>
2024-04-04 20:25:02 +0800

    Updated main.py
    Largely improved chatbot interactivity by providing additional information (if applicable) and creating more report fields for the chatbot to include and compile.

--- main.py
+++ main.py
@@ -2,12 +2,19 @@ from duckduckgo_search import DDGS
 from g4f.client import Client
 
 word = input("Query: ")
+add = input("Any other information: ")
 new = '"' + word + '"'
-results = DDGS().text(new, max_results=100)
+results = DDGS().text(new, max_results=200)
+
+    
+print(results)
 
 
-payload =  "If I gave you some information on someone, could you write me a report on them?"
-payload += " Here is the data with some helpful links. Do note that the person's name is" + word + ", perhaps you could also create a web of people relating to " + word + ", and provide the links where the information can be found"
+payload =  "If I gave you some information on someone, could you write me a comprehensive report on them?"
+payload += " Do note that the person's name is" + word + ", perhaps you could also create a web of people relating to " + word + ", and provide the links where the information can be found."
+payload += "Do also see if you can extract basic information such as past and current education statuses, location and information from certain accounts like their username or email."
+payload += "Lastly, also note that " + add 
+payload += " Here is the data with links: \n."
 
 gptstring = ""
 
@@ -25,8 +32,5 @@ response = client.chat.completions.create(
 print("")
 print(response.choices[0].message.content)
 
-#MIT licence
-
 # Special thanks to:
 # https://github.com/xtekky/gpt4free, for providing free gpt api
-
