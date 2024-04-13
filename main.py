546c0a741680b08c27a274b6ae252a5062c375cf
AnonymousRendezvous <92633933+AnonymousRendezvous@users.noreply.github.com>
2024-04-13 22:55:50 +0800

    Update main.py
    Added threading --> largely increased details and accuracy, however program is still quite slow so multiprocessing might be needed.

--- main.py
+++ main.py
@@ -2,27 +2,29 @@
 from duckduckgo_search import DDGS
 # pip install -U g4f
 from g4f.client import Client
+import threading
 
 # Important to note that only one query to GPT is available per request --> might need to integrate threading
 
 word = input("Query: ")
 add = input("Any other information: ")
 img = input("Do you want to trawl for images? (beta testing) y/n: ")
+details = int(input("How detailed do you want your report to be? (1-10): "))
 new = '"' + word + '"'
 
 if img == "y":
-    results = DDGS().text(new, max_results=30)
-    images = DDGS().images(word, region="sg-en", max_results=20)
+    results = DDGS().text(new, max_results=100)
+    images = DDGS().images(new, region="sg-en", max_results=100)
 else:
-    results = DDGS().text(new, max_results=50)
+    results = DDGS().text(new, max_results=200)
 
 
 #payload for first search
 payload =  "If I gave you some information on someone, could you write me a comprehensive report on them?"
-payload += " Do note that the person's name is" + word + ", perhaps you could also create a web of people relating to " + word + ", and provide the links where the information can be found."
+payload += " Do note that the person's name is" + word + ", perhaps you could also create a web of people relating to " + word + ", and provide ANY and ALL links where the information can be found."
 payload += "Do also see if you can extract basic information such as past and current education statuses, location and information from certain accounts like their username or email."
 payload += "Lastly, also note that " + add 
-payload += " Here is the data with links: \n."
+payload += " Here is the data with links (to be included): \n."
 
 gptstring = ""
 # refined gpt string
@@ -57,13 +59,33 @@ if img == "y":
 else:
   finalpayload += payload
 
-client = Client()
-response = client.chat.completions.create(
-    model = "gpt-3.5-turbo",
-    messages=[{"role": "user", "content": finalpayload}]
-)
+storenum = 0
+storemsg = ""
+
+def chat(finalpayload):
+    client = Client()
+    response = client.chat.completions.create(
+        model = "gpt-3.5-turbo",
+        messages=[{"role": "user", "content": finalpayload}]
+    )
+    #accessing global values outside function
+    global storenum
+    global storemsg
+    if len(response.choices[0].message.content) > storenum:
+        storenum = len(response.choices[0].message.content)
+        storemsg = response.choices[0].message.content
+
+    
+# Threading implementation (submit multiple response to chat gpt)
+for x in range(details):
+    t1 = threading.Thread(target=chat, args=(finalpayload,))
+    t1.start()
+    t1.join()
+
+    print(x)
+
 print("")
-print(response.choices[0].message.content)
+print(storemsg)
 
 # Special thanks to:
 # https://github.com/xtekky/gpt4free, for providing free gpt 3.5 api
