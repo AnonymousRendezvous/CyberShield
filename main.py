e135d7bbdae07aa0838b7fe4dc0707f6e1979962
AnonymousRendezvous <92633933+AnonymousRendezvous@users.noreply.github.com>
2024-04-24 20:33:36 +0800

    Refactor main.py
    Refactored code and added some functions for organisation in the future.

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ word = input("Query: ")
 add = input("Any other information: ")
 img = input("Do you want to trawl for images? (beta testing) y/n: ")
 details = int(input("How detailed do you want your report to be? (1-10): "))
+
 new = '"' + word + '"'
 
 if img == "y":
@@ -22,49 +23,47 @@ else:
     results = DDGS().text(new, max_results=50)
 
 
-#payload for first search
-payload =  "If I gave you some information on someone, could you write me a comprehensive report on them?"
-payload += " Do note that the person's name is" + word + ", perhaps you could also create a web of people relating to " + word + ", and provide ANY and ALL links where the information can be found."
-payload += "Do also see if you can extract basic information such as past and current education statuses, location and information from ANY accounts like their username or email."
-payload += "Lastly, also note that " + add 
-payload += " Here is the data with links (to be included): \n."
-
-gptstring = ""
-# refined gpt string
-refgptstr = ""
-
-#iterating through dictionary
-
-if img == "y":
-    for x in results:
-        gptstring += ' '
-        gptstring += str(x)
-
-    for x in images:
-        refgptstr += ' '
-        refgptstr += str(x)
-else:
-    for x in results:
-        gptstring += ' '
-        gptstring += str(x)
-
-
-payload += " Data: " + gptstring
-
-#payload for images
-payloadimg = "Now, with the above data, I will provide you with some image links relating to " + word
-payloadimg += ". Do filter through all the data and give me ANY relevant links. Here is the data: " + refgptstr
+#Global variables
 
 finalpayload = ""
-
-if img == "y":
-  finalpayload += payload + " " + payloadimg
-else:
-  finalpayload += payload
-
 storenum = 0
 storemsg = ""
 
+#function for generating final payload
+def payload_gen(word, add):
+    global finalpayload
+    payload = f"If I gave you some information on someone, could you write me a comprehensive report on them?"
+    payload += f" Do note that the person's name is, {word}. Perhaps you could also create a web of people relating to {word} and provide ANY and ALL links where the information can be found."
+    payload += "Do also see if you can extract basic information such as past and current education statuses, location and information from ANY accounts like their username or email."
+    payload += f"Lastly, also note that {add}  Here is the data with links (to be included): \n."
+    gptstring = ""
+    refgptstr = ""
+    if img == "y":
+        for x in results:
+            gptstring += ' '
+            gptstring += str(x)
+
+        for x in images:
+            refgptstr += ' '
+            refgptstr += str(x)
+
+        payload += " Data: " + gptstring
+        payloadimg = f" Now, with the above data, I will provide you with some image links relating to {word}."
+        payloadimg += f" Do filter through all the data and give me ANY relevant links. Here is the data: {refgptstr}."
+        finalpayload += payload + " " + payloadimg
+    else:
+        for x in results:
+            gptstring += ' '
+            gptstring += str(x)
+
+        payload += " Data: " + gptstring
+        finalpayload += payload
+
+
+
+
+
+#function for generating response
 def chat(finalpayload):
     client = Client()
     response = client.chat.completions.create(
@@ -79,9 +78,12 @@ def chat(finalpayload):
         storemsg = str(response.choices[0].message.content)
 
 
+payload_gen(word, add)
+
 # Threading implementation (submit multiple responses to chat gpt)
 for x in range(details):
     print("This will take a while, please wait...")
+    # to fix coro async runtime error in future
     t1 = threading.Thread(target=chat, args=(finalpayload,))
     t2 = threading.Thread(target=chat, args=(finalpayload,))
     t3 = threading.Thread(target=chat, args=(finalpayload,))
