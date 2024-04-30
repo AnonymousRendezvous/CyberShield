3c0bbaf458841c96042e42aac24e50d6fb38c3fb
AnonymousRendezvous <92633933+AnonymousRendezvous@users.noreply.github.com>
2024-04-30 22:05:21 +0800

    Added GUI to main.py
    New GUI implemented with tkinter. Android testing still underway as with reimplementation of multithreading. Will refactor code to display output to GUI screen in next iteration.

--- main.py
+++ main.py
@@ -1,36 +1,51 @@
-# pip install duckduckgo_search
+import tkinter as tk
+from tkinter import simpledialog
 from duckduckgo_search import DDGS
+import threading
 # pip install -U g4f
 from g4f.client import Client
-import threading
 import sys
 import os
 
-# Suppress stderr output by redirecting it to os.devnull
 sys.stderr = open(os.devnull, 'w')
 
-word = input("Query: ")
-add = input("Any other information: ")
-img = input("Do you want to trawl for images? (beta testing) y/n: ")
-details = int(input("How detailed do you want your report to be? (1-10): "))
+class GetUserNamePassword(simpledialog.Dialog):
+    def body(self, master):
+        tk.Label(master, text="Query:").grid(row=0)
+        tk.Label(master, text="Any other information:").grid(row=1)
+        tk.Label(master, text="Do you want to trawl for images? (beta testing) y/n: ").grid(row=2)
+        tk.Label(master, text="How detailed do you want the report to be? (1-10):").grid(row=3)
 
-new = '"' + word + '"'
+        self.e0 = tk.Entry(master)
+        self.e1 = tk.Entry(master)
+        self.e2 = tk.Entry(master)
+        self.e3 = tk.Entry(master)
 
-if img == "y":
-    results = DDGS().text(new, max_results=25)
-    images = DDGS().images(new, region="sg-en", max_results=25)
-else:
-    results = DDGS().text(new, max_results=50)
+        self.e0.grid(row=0, column=1)
+        self.e1.grid(row=1, column=1)
+        self.e2.grid(row=2, column=1)
+        self.e3.grid(row=3, column=1)
 
-
-#Global variables
+    def apply(self):
+        self.result = (
+            self.e0.get(),
+            self.e1.get(),
+            self.e2.get(),
+            self.e3.get(),
+        )
 
 finalpayload = ""
 storenum = 0
 storemsg = ""
 
-#function for generating final payload
-def payload_gen(word, add):
+def payload_gen(word, add, img):
+    new = '"' + word + '"'
+
+    if img == "y":
+        results = DDGS().text(new, max_results=25)
+        images = DDGS().images(new, region="sg-en", max_results=25)
+    else:
+        results = DDGS().text(new, max_results=50)
     global finalpayload
     payload = f"If I gave you some information on someone, could you write me a comprehensive report on them?"
     payload += f" Do note that the person's name is, {word}. Perhaps you could also create a web of people relating to {word} and provide ANY and ALL links where the information can be found."
@@ -59,41 +74,27 @@ def payload_gen(word, add):
         payload += " Data: " + gptstring
         finalpayload += payload
 
-
-
-
-
-#function for generating response
-def chat(finalpayload):
+storemsg = "h"
+def chat(finalpayload, storemsg):
     client = Client()
-    response = client.chat.completions.create(
-        model = "airoboros-70b",
+    storemsg = client.chat.completions.create(
+        model = "gpt-3.5-turbo",
         messages=[{"role": "user", "content": finalpayload}]
     )
+    print(storemsg.choices[0].message.content) 
     #accessing global values outside function
-    global storenum
-    global storemsg
-    if len(response.choices[0].message.content) > storenum:
-        storenum = len(response.choices[0].message.content)
-        storemsg = str(response.choices[0].message.content)
 
 
-payload_gen(word, add)
+def main():
+    root = tk.Tk()
+    root.withdraw()  # Hide the root window
+    d = GetUserNamePassword(root)
+    if d.result:  # Check if the dialog returned a result
+        word, add, img, details = d.result
+        # Run the function in a separate thread
+        payload_gen(word, add, img)
+        chat(finalpayload, storemsg)
+    else:
+        print("failed")
 
-# Threading implementation (submit multiple responses to chat gpt)
-for x in range(details):
-    print("This will take a while, please wait...")
-    # to fix coro async runtime error in future
-    t1 = threading.Thread(target=chat, args=(finalpayload,))
-    t2 = threading.Thread(target=chat, args=(finalpayload,))
-    t3 = threading.Thread(target=chat, args=(finalpayload,))
-    t1.start()
-    t2.start()
-    t3.start()
-
-    t1.join()
-    t2.join()
-    t3.join()
-
-print("")
-print(storemsg)
+main()
